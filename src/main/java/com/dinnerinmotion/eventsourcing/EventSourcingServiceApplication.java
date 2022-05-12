96919a550b30618bd9442ad1ba0dcf4f7f384681
CalvinMoonly <calvin@moonlysoftware.com>
2022-05-12 14:47:51 +0200

    connect to confluent

--- src/main/java/com/dinnerinmotion/eventsourcing/EventSourcingServiceApplication.java
+++ src/main/java/com/dinnerinmotion/eventsourcing/EventSourcingServiceApplication.java
@@ -3,25 +3,22 @@ package com.dinnerinmotion.eventsourcing;
 import com.dinnerinmotion.eventsourcing.controller.EventController;
 import com.dinnerinmotion.eventsourcing.entity.Event;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.retry.annotation.Backoff;
 
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootApplication
 @EnableEurekaClient
 @Slf4j
 public class EventSourcingServiceApplication {
-	private final Logger logger = LoggerFactory.getLogger(EventSourcingServiceApplication.class);
+	private final String groupKafka = "eventSourcing";
 
 	@Autowired
 	private EventController eventController;
@@ -30,18 +27,15 @@ public class EventSourcingServiceApplication {
 		SpringApplication.run(EventSourcingServiceApplication.class, args);
 	}
 
-	@RetryableTopic(attempts = "5", backoff = @Backoff(delay = 2_000, maxDelay = 10_000, multiplier = 2))
-	@KafkaListener(id = "eventGroup", topics = "mockTopic")
-	public void listen(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-					   @Header(KafkaHeaders.OFFSET) long offset) {
+	@KafkaListener(id = groupKafka, topics = "mockTopic")
+	public void processMessage(String message,
+							   @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List partitions,
+							   @Header(KafkaHeaders.RECEIVED_TOPIC) List topics,
+							   @Header(KafkaHeaders.OFFSET) List offsets) {
 
-		this.logger.info("Received: {} from {} @ {}", in, topic, offset);
+		System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
 
-		if (in.startsWith("fail")) {
-			throw new RuntimeException("failed");
-		}
-
-		Event newEvent = new Event(UUID.randomUUID(), topic, in);
+		Event newEvent = new Event(UUID.randomUUID(), (String) topics.get(0), message);
 		eventController.createEvent(newEvent);
 	}
 }
