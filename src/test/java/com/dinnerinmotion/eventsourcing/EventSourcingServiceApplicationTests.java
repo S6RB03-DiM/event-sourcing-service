a6a21d79ac06e3ab6d2e155c63bcb3c45bdd0379
Dimitar Petrov <d.petrov-ab@student.fontys.nl>
2022-06-13 15:22:05 +0200

    add test

--- src/test/java/com/dinnerinmotion/eventsourcing/EventSourcingServiceApplicationTests.java
+++ src/test/java/com/dinnerinmotion/eventsourcing/EventSourcingServiceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.dinnerinmotion.eventsourcing;
 
+import com.dinnerinmotion.eventsourcing.entity.Event;
+import com.dinnerinmotion.eventsourcing.service.EventService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import java.util.UUID;
 
 @SpringBootTest
 class EventSourcingServiceApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Autowired
+	EventService eventService;
 
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void eventIsSavedToRepository() {
+		Event event = new Event(UUID.fromString("5e7e1f40-da91-11ec-9d64-0242ac120002"), "topic", "payload");
+		event.setEventId(1L);
+
+		eventService.saveEvent(event);
+		assertEquals(eventService.findEventById(1L), event);
+	}
 }
